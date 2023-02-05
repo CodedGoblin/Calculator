4c21c31870ded1e505840fc40549cc4b34e51f86
CodedGoblin <athmane1naili@gmail.com>
2023-02-05 11:50:05 +0100

    finished core script with all base functionalites

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// global Variables
+let a = '';
+let b = '';
+let op = '';
+let result = '';
+let displayValue = '_ _ _';
+
+// Selections
+const display = document.querySelector('.display');
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector('#equal');
+const clear = document.querySelector('#clear');
+
+
 // addition
 const add = (a,b)=>{
     return a+b;
@@ -23,4 +38,67 @@ const operate = (a,b,operator)=>{
 };
 
 
+// Populate the display with the buttons pressed eg: 1 + 2 
+// Save first value as 'a', save operator chosen, and save second value as 'b'
+    // Digit Functionality
+digits.forEach(digit=>{
+    digit.addEventListener('click', ()=>{
+        if(!op){
+            a += digit.textContent;
+            displayValue = a;
+        } else{
+            b += digit.textContent;
+            displayValue = b;
+        }
+        view(displayValue);
+    });
+});
+    // Operation Functionality
+operators.forEach(operator =>{
+    operator.addEventListener('click', ()=>{
+        op = operator.textContent;
+        if(!b == ''){
+            result = operate(Number(a),Number(b),select(op));
+            a = result;
+            displayValue = a;
+            b = ''
+        }
+        view(displayValue);
+    })
+})
 
+    // Equal Func
+equal.addEventListener('click', ()=>{
+    result = operate(Number(a),Number(b),select(op));
+    displayValue = result;
+    view(result)
+    a = result;
+    b = '';
+    result = '';  
+})
+
+    // Clear Functionality
+clear.addEventListener('click', ()=>{
+    displayValue = '_ _ _';
+    a = '';
+    b = '';
+    op = '';
+    result = '';
+    view(displayValue);
+})
+
+
+
+// Utility functions
+    // Display Functionality
+const view = (value)=>{
+    display.textContent = value;
+};
+
+    // Select the corresponding operator
+const select = (op)=>{
+    return op == '+' ? add
+          :op == '-' ? substract
+          :op == '*' ? multiply
+          :op == '/' ? divide : '';
+};
