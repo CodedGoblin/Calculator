1e1557e626c715030ae8532beabe3c90b40dec49
CodedGoblin <athmane1naili@gmail.com>
2023-02-10 12:06:02 +0100

    Refactored and fixed bugs

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // global Variables
-let a = '';
+let a = 0;
 let b = '';
 let op = '';
 let result = 0;
+
 
 
 // Selections
@@ -11,6 +12,7 @@ const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 const equal = document.querySelector('#equal');
 const clear = document.querySelector('#clear');
+const decimal = document.querySelector('#decimal');
 
 
 // addition
@@ -33,64 +35,70 @@ const divide = (a,b)=>{
 
 // operate takes (a,b,opeator) and returns result
 const operate = (a,b,operator)=>{
-    const result = operator(a,b);
-    return result;
+    return operator(Number(a),Number(b));
 };
 
 
-// Populate the display with the buttons pressed 
-// Save first value as 'a', save operator chosen, and save second value as 'b'
+
+
+
     // Digit Functionality
 digits.forEach(digit=>{
     digit.addEventListener('click', ()=>{
-        view('');
-        if(!op){
-            a += digit.textContent;
-            view(a);
-        } else{
+        if(!op == ''){
             b += digit.textContent;
-            result = parseFloat(operate(Number(a),Number(b),select(op)).toFixed(2));
-            a = result;
-            view(b);
-        }
+            view(b)
+        }else{
+            a += digit.textContent;
+            view(a)
+        }         
     });
 });
+
+// digit || operator : if digit=assign 'a'                      if operator=assign 'b' 
+//                     press operator                           result 
+//                     assign 'b'                               put result in a 
+//                     op or eq                                 op or eq
+//                     result                                   empty b an op
+//                     put result in a                          
+//                     empty b and op
+//                     reset from 3rd line
 
 
     // Operation Functionality
 operators.forEach(operator =>{
     operator.addEventListener('click', ()=>{
-        view(checkInfinity(result)); 
-        op = operator.textContent;
-        if(!a){
-            op = '';
-        }else{
-            if(!b == ''){
-                b = ''
-                view(result);
-            }else{
-                result = a;
-                view(result);
-            }
-        }
+       op = operator.textContent;
+       if(!b==''){
+        result = parseFloat(operate(a,b,select(op)).toFixed(2));
+        view(checkInfinity(result));
+        a = result;
+        b = '';
+        op = '';
+       }
     })
 })
 
     // Equal Func
 equal.addEventListener('click', ()=>{ 
-    if(!a){
-        view(0);
+    if(b == ''){ // safety in case the equal is pressed before op and b are assigned
+        view(a)
+    }else{
+        result = parseFloat(operate(a,b,select(op)).toFixed(2));
+        view(checkInfinity(result));
+        a = result;
+        b = '';
+        op = '';  
     }
-    view(checkInfinity(result));   
 })
 
     // Clear Functionality
 clear.addEventListener('click', ()=>{
-    a = '';
+    a = 0;
     b = '';
     op = '';
     result = '';
-    view(0);
+    view(a)
 })
 
 
@@ -98,7 +106,7 @@ clear.addEventListener('click', ()=>{
 // Utility functions
     // Display Functionality
 const view = (value)=>{
-    display.textContent = value;
+    isNaN(value) ? display.textContent = value : display.textContent = Number(value)
 };
 
     // Select the corresponding operator
@@ -112,11 +120,20 @@ const select = (op)=>{
 
     // Check for inifity
 const checkInfinity = (result)=>{
-    if (result == 'Infinity'){
-        a=''
-        b=''
-        return 'Not Possible'
-
+    if (result == Infinity){
+        a=0;
+        op='';
+        b='';
+        return 'Not Possible';
     }
     return result
 }
+
+
+
+    // check dot
+
+decimal.addEventListener('click', ()=>{
+   if(!a.toString().includes('.')) a+='.';
+   if(!op == '' && !b.includes('.')) b+='.'
+})
